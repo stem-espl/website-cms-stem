3f641176d1f911d59e0c0eb0951bbced9fd76dc8
PrashantEncureIT <96293606+PrashantEncureIT@users.noreply.github.com>
2024-04-25 19:02:04 +0530

    dynamic header with logo, favicon, website heading

--- alter.sql
+++ alter.sql
@@ -4,3 +4,5 @@ INSERT INTO `permissions` (`id`, `name`, `guard_name`, `group_name`, `status`, `
 INSERT INTO `permissions` (`id`, `name`, `guard_name`, `group_name`, `status`, `basic`, `created_at`, `updated_at`) VALUES (NULL, 'contact', 'admin', 'contact', '1', '0', '2024-04-19 14:45:27', '2024-04-19 14:45:27'); 
 INSERT INTO `permissions` (`id`, `name`, `guard_name`, `group_name`, `status`, `basic`, `created_at`, `updated_at`) VALUES (NULL, 'audit-trail', 'admin', 'audit_trail', '1', '0', '2024-04-19 14:45:27', '2024-04-19 14:45:27'); 
 INSERT INTO `permissions` (`id`, `name`, `guard_name`, `group_name`, `status`, `basic`, `created_at`, `updated_at`) VALUES (NULL, 'announcement-popup', 'admin', 'announcement_popup', '1', '0', '2024-04-19 14:45:27', '2024-04-19 14:45:27'); 
+-- Date: 25/04/2024
+ALTER TABLE `basic_settings` CHANGE `website_heading` `website_heading` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
