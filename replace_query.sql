11949251b8cebdafa1251e5a440b9752fe63d349
PrashantEncureIT <96293606+PrashantEncureIT@users.noreply.github.com>
2024-05-13 17:35:46 +0530

    asset files

--- replace_query.sql
+++ replace_query.sql
@@ -1,12 +1,12 @@
-UPDATE basic_settings SET intro_section_button_url = REPLACE (intro_section_button_url, 'http://localhost:8001', 'http://localhost:8002');
+UPDATE basic_settings SET intro_section_button_url = REPLACE (intro_section_button_url, 'http://localhost:8002', 'http://localhost:8001');
 
-UPDATE basic_settings SET hero_section_button_url = REPLACE (hero_section_button_url, 'http://localhost:8001', 'http://localhost:8002');
+UPDATE basic_settings SET hero_section_button_url = REPLACE (hero_section_button_url, 'http://localhost:8002', 'http://localhost:8001');
 
-UPDATE basic_settings SET cta_section_button_url = REPLACE (cta_section_button_url, 'http://localhost:8001', 'http://localhost:8002');
+UPDATE basic_settings SET cta_section_button_url = REPLACE (cta_section_button_url, 'http://localhost:8002', 'http://localhost:8001');
 
-UPDATE menus SET menus = REPLACE (menus, 'localhost:8001', 'localhost:8002');
+UPDATE menus SET menus = REPLACE (menus, 'localhost:8002', 'localhost:8001');
 UPDATE menus SET menus = REPLACE (menus, 'http', 'http');
 
-UPDATE ulinks SET url = REPLACE (url, 'localhost:8001', 'localhost:8002');
-UPDATE dlink SET url = REPLACE (url, 'localhost:8001', 'localhost:8002');
-UPDATE alink SET url = REPLACE (url, 'localhost:8001', 'localhost:8002');
+UPDATE ulinks SET url = REPLACE (url, 'localhost:8002', 'localhost:8001');
+UPDATE dlink SET url = REPLACE (url, 'localhost:8002', 'localhost:8001');
+UPDATE alink SET url = REPLACE (url, 'localhost:8002', 'localhost:8001');
